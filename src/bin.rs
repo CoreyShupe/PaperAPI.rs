cf62cc13e7afdcaa9da5d0e3dad134accc4e3e75
CoreyShupe <corey.shupe@superleague.com>
2020-12-06 22:59:18 -0500

    Do a bit of code cleanup.

--- src/bin.rs
+++ src/bin.rs
@@ -5,6 +5,7 @@ extern crate clap;
 
 use paper_api::{PaperClientDebug, PaperClientConfig, PaperClient};
 use clap::ArgMatches;
+use paper_api::paper::ChangesInfo;
 
 #[tokio::main]
 async fn main() {
@@ -36,174 +37,6 @@ async fn main() {
     } else {
         handle_matches::<PaperClient>(app_matcher).await;
     }
-
-
-    // match app_matcher.subcommand_name() {
-    //     Some("projects") => {
-    //         let response = Client::get_projects().await;
-    //         match response {
-    //             Ok(projects) => {
-    //                 println!("Projects: \t{:?}", projects.projects);
-    //             }
-    //             Err(e) => {
-    //                 println!("Error: {}", e);
-    //             }
-    //         }
-    //     }
-    //     Some("project") => {
-    //         let project_command = app_matcher.subcommand_matches("project")
-    //             .expect("Sub command must be \"project\".");
-    //
-    //         let project = project_command.value_of("project").expect("Arg project required.");
-    //         let version = project_command.value_of("version");
-    //         let group = project_command.value_of("group");
-    //
-    //         match project_command.subcommand_name() {
-    //             Some("builds") => {
-    //                 let build_command = project_command.subcommand_matches("builds")
-    //                     .expect("Sub command must be \"builds\".");
-    //
-    //                 let build = build_command.value_of("build");
-    //                 if let Some(v) = version {
-    //                     if let Some(b) = build {
-    //                         let build_i32 = b.parse::<i32>();
-    //                         if let Ok(b_i32) = build_i32 {
-    //                             let response = Client::get_version_builds(project, v, b_i32).await;
-    //                             match response {
-    //                                 Ok(info) => {
-    //                                     println!("Changes:");
-    //                                     for change_info in info.changes {
-    //                                         println!("\tCommit: {}", change_info.commit);
-    //                                         println!("\tMessage: \n`\n{}\n`", change_info.message);
-    //                                         println!("\tSummary: {}", change_info.summary);
-    //                                         println!();
-    //                                     }
-    //                                     println!("Project ID:    \t{}", info.project_id);
-    //                                     println!("Project Name:  \t{}", info.project_name);
-    //                                     println!("Version:       \t{}", info.version);
-    //                                     println!("Time:          \t{}", info.time);
-    //                                     println!("{:?}", info.downloads);
-    //                                 }
-    //                                 Err(e) => {
-    //                                     println!("Error: {}", e);
-    //                                 }
-    //                             }
-    //                         } else {
-    //                             println!("Build must be a number.");
-    //                         }
-    //                     } else {
-    //                         let response = Client::get_version_info(project, v).await;
-    //                         match response {
-    //                             Ok(info) => {
-    //                                 println!("Project ID:   \t{}", info.project_id);
-    //                                 println!("Project Name: \t{}", info.project_name);
-    //                                 println!("Version:      \t{}", info.version);
-    //                                 println!("Builds:       \t{:?}", info.builds);
-    //                             }
-    //                             Err(e) => {
-    //                                 println!("Error: {}", e);
-    //                             }
-    //                         }
-    //                     }
-    //                 } else if let Some(g) = group {
-    //                     let response = Client::get_group_builds(project, g).await;
-    //                     match response {
-    //                         Ok(info) => {
-    //                             if let Some(b) = build {
-    //                                 let build_i32 = b.parse::<i32>();
-    //
-    //                                 if let Ok(b_i32) = build_i32 {
-    //                                     println!("Project ID:    \t{}", info.project_id);
-    //                                     println!("Project Name:  \t{}", info.project_name);
-    //                                     println!("Version Group: \t{}", info.version_group);
-    //                                     println!("Versions:      \t{:?}", info.versions);
-    //                                     let mut flag = false;
-    //                                     for build_info in info.builds {
-    //                                         if build_info.build == b_i32 {
-    //                                             println!("Changes:");
-    //                                             for change_info in build_info.changes {
-    //                                                 println!("\tCommit: {}", change_info.commit);
-    //                                                 println!("\tMessage: \n`\n{}\n`", change_info.message);
-    //                                                 println!("\tSummary: {}", change_info.summary);
-    //                                                 println!();
-    //                                             }
-    //                                             println!("Time:          \t{}", build_info.time);
-    //                                             println!("Version:       \t{}", build_info.version);
-    //                                             println!("{:?}", build_info.downloads);
-    //                                             flag = true;
-    //                                         }
-    //                                     }
-    //                                     if !flag {
-    //                                         println!("Build not found in this group.");
-    //                                     }
-    //                                 } else {
-    //                                     println!("Build must be a number.");
-    //                                 }
-    //                             } else {
-    //                                 println!("Project ID:    \t{}", info.project_id);
-    //                                 println!("Project Name:  \t{}", info.project_name);
-    //                                 println!("Version Group: \t{}", info.version_group);
-    //                                 println!("Versions:      \t{:?}", info.versions);
-    //                                 for build_info in info.builds {
-    //                                     println!("\t{}: {} at {}", build_info.version, build_info.build, build_info.time)
-    //                                 }
-    //                             }
-    //                         }
-    //                         Err(e) => {
-    //                             println!("Error: {}", e);
-    //                         }
-    //                     }
-    //                 } else {
-    //                     println!("Group or version required to display build information.")
-    //                 }
-    //             }
-    //             None => {
-    //                 if let Some(v) = version {
-    //                     let response = Client::get_version_info(project, v).await;
-    //                     match response {
-    //                         Ok(info) => {
-    //                             println!("Project ID:   \t{}", info.project_id);
-    //                             println!("Project Name: \t{}", info.project_name);
-    //                             println!("Version:      \t{}", info.version);
-    //                             println!("Builds:       \t{:?}", info.builds);
-    //                         }
-    //                         Err(e) => {
-    //                             println!("Error: {}", e);
-    //                         }
-    //                     }
-    //                 } else if let Some(g) = group {
-    //                     let response = Client::get_group_info(project, g).await;
-    //                     match response {
-    //                         Ok(info) => {
-    //                             println!("Project ID:    \t{}", info.project_id);
-    //                             println!("Project Name:  \t{}", info.project_name);
-    //                             println!("Version Group: \t{}", info.version_group);
-    //                             println!("Versions:      \t{:?}", info.versions);
-    //                         }
-    //                         Err(e) => {
-    //                             println!("Error: {}", e);
-    //                         }
-    //                     }
-    //                 } else {
-    //                     let response = Client::get_project(project).await;
-    //                     match response {
-    //                         Ok(project_info) => {
-    //                             println!("Project ID:             \t{}", project_info.project_id);
-    //                             println!("Project Name:           \t{}", project_info.project_name);
-    //                             println!("Project Version Groups: \t{:?}", project_info.version_groups);
-    //                             println!("Project Versions:       \t{:?}", project_info.versions);
-    //                         }
-    //                         Err(e) => {
-    //                             println!("Error: {}", e);
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //             _ => unreachable!()
-    //         }
-    //     }
-    //     _ => unreachable!()
-    // };
 }
 
 async fn handle_matches<T>(matcher: ArgMatches<'_>)
@@ -242,13 +75,7 @@ async fn handle_matches<T>(matcher: ArgMatches<'_>)
                                 let response = T::get_version_builds(project, v, b_i32).await;
                                 match response {
                                     Ok(info) => {
-                                        println!("Changes:");
-                                        for change_info in info.changes {
-                                            println!("\tCommit: {}", change_info.commit);
-                                            println!("\tMessage: \n`\n{}\n`", change_info.message);
-                                            println!("\tSummary: {}", change_info.summary);
-                                            println!();
-                                        }
+                                        print_changes(&info.changes);
                                         println!("Project ID:    \t{}", info.project_id);
                                         println!("Project Name:  \t{}", info.project_name);
                                         println!("Version:       \t{}", info.version);
@@ -291,17 +118,12 @@ async fn handle_matches<T>(matcher: ArgMatches<'_>)
                                         let mut flag = false;
                                         for build_info in info.builds {
                                             if build_info.build == b_i32 {
-                                                println!("Changes:");
-                                                for change_info in build_info.changes {
-                                                    println!("\tCommit: {}", change_info.commit);
-                                                    println!("\tMessage: \n`\n{}\n`", change_info.message);
-                                                    println!("\tSummary: {}", change_info.summary);
-                                                    println!();
-                                                }
+                                                print_changes(&build_info.changes);
                                                 println!("Time:          \t{}", build_info.time);
                                                 println!("Version:       \t{}", build_info.version);
                                                 println!("{:?}", build_info.downloads);
                                                 flag = true;
+                                                break;
                                             }
                                         }
                                         if !flag {
@@ -375,4 +197,14 @@ async fn handle_matches<T>(matcher: ArgMatches<'_>)
         }
         _ => unreachable!()
     };
+}
+
+fn print_changes(info: &Vec<ChangesInfo>) {
+    println!("Changes:");
+    for change_info in info {
+        println!("\tCommit: {}", change_info.commit);
+        println!("\tMessage: \n`\n{}\n`", change_info.message);
+        println!("\tSummary: {}", change_info.summary);
+        println!();
+    }
 }
